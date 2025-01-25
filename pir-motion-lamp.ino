747b6ee39695a82dfa753bdac01a98daf3a5fd1a
Lorenz Adam Damara <lorenzrenz@gmail.com>
2025-01-25 13:52:18 +0700

    read PIR value

--- pir-motion-lamp.ino
+++ pir-motion-lamp.ino
@@ -1,7 +1,13 @@
+const int pirPin = 12;
+const int relayPin = 18;
+
 void setup(){
-    
+    pinMode(pirPin, INPUT);
+
+    Serial.begin(9600);
 }
 
 void loop(){
-
+    bool statusPir = digitalRead(pirPin);
+    Serial.println(statusPir);
 }

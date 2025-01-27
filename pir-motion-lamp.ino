a5da0b311e0496fecb3466c049761d62f2d3cef9
Lorenz Adam Damara <lorenzrenz@gmail.com>
2025-01-27 17:04:51 +0700

    connect wifi

--- pir-motion-lamp.ino
+++ pir-motion-lamp.ino
@@ -1,16 +1,42 @@
+#include <WiFi.h>
+
+const char* ssid = "Wokwi-GUEST";
+const char* password = "";
+
 const int pirPin = 12;
 const int relayPin = 18;
+const int redPin = 27;
+const int greenPin = 26;
+const int bluePin = 25;
+
+void connect(){
+    rgb(1,0,0);
+    while(WiFi.status() != WL_CONNECTED){
+        delay(500);
+    }
+    rgb(0,1,0);
+}
 
 void setup(){
     pinMode(pirPin, INPUT);
     pinMode(relayPin, OUTPUT);
+    pinMode(redPin, OUTPUT);
+    pinMode(greenPin, OUTPUT);
+    pinMode(bluePin, OUTPUT);
 
-    Serial.begin(9600);
+    WiFi.begin(ssid, password);
+
+    connect();
 }
 
 void loop(){
     bool statusPir = digitalRead(pirPin);
-    Serial.println(statusPir);
 
     digitalWrite(relayPin, statusPir);
+}
+
+void rgb(bool red, bool green, bool blue){
+    digitalWrite(redPin, red);
+    digitalWrite(greenPin, green);
+    digitalWrite(bluePin, blue);
 }
